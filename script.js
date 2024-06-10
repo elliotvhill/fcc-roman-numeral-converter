3b9df6f992f38393e243d938a61a6fca07642591
Elliot <ellthrowaway2@icloud.com>
2024-06-10 17:19:36 -0400

    working through logic, identifying recursive and base cases

--- script.js
+++ script.js
@@ -16,10 +16,41 @@ const checkInput = () => {
         output.classList.toggle("hidden");
         };
         
-        const arabicToRoman = (input) => {
-            const number = input
-            console.log(`Arabic number is: ${number}`);
-}
+const arabicToRoman = (input) => {
+    const arabicNumber = input;
+    let remainder = null;
+    console.log(`Arabic number is: ${arabicNumber}`);
+    if (arabicNumber % 10 === 0) {
+        let quotient = arabicNumber / 10; // number of X's to display
+        console.log("Quotient: ", quotient)
+        if (remainder % 5 === 0) {
+            remainder = remainder / 5;
+        console.log("Remainder: ", remainder)
+        return remainder;
+        }
+        return quotient;
+    } else {
+        let remainder = arabicNumber % 10;
+        if (remainder % 5 === 0) {
+            remainder = remainder / 5;
+        }
+        console.log("Remainder: ", remainder)
+        return remainder;
+    };
+
+
+    // recursive case: n % 5 === 0
+    
+    // base case: n % 5 !== 0
+
+
+
+    // is n divisible by 5 ? --------- RECURSIVE CASE
+        //// is (n / 5) divisible by 5 ?
+            //// etc.....
+    // when n % 5 !== 0, calculate remainder --------- BASE CASE
+
+};
 
 
 // EVENT LISTENERS //
