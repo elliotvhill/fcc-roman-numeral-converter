6db860de30337a3c3a7338755d4b9a0931f9aa3b
Elliot <ellthrowaway2@icloud.com>
2024-06-10 15:59:04 -0400

    added button text; starting to build app logic

---
+++ script.js
@@ -0,0 +1,16 @@
+// VARIABLES //
+const input = document.getElementById("number")
+const convertBtn = document.getElementById("convert-btn")
+const output = document.getElementById("output")
+
+// FUNCTIONS //
+
+const checkInput = () => {
+    const numberInput = input.value
+    console.log(numberInput)
+    }
+
+// EVENT LISTENERS //
+
+convertBtn.addEventListener("click", checkInput)
+
