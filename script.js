544f864f99328b15f49ce61349119b1cad0b490c
Elliot <ellthrowaway2@icloud.com>
2024-06-12 14:42:56 -0400

    cleaned up branch code a little

--- script.js
+++ script.js
@@ -32,44 +32,33 @@ const checkInput = () => {
         arabicToRoman(numberInput);
     }
     output.classList.remove('hidden');
-    // input.value = '';
 };
 
 const arabicToRoman = (input) => {
-    // let remainder = null;
+    
 
-    // convert input num to all roman I's √√
-    // for (let i = input; i > 0; i--){
-    //     numeralsArray.push("I")
-    //     };
-
-    while (input > 0) {
-        let quotient = Math.floor(input / 5); // number of V's needed
-        while (quotient != 0) {
-            numeralsArray.push('I');
-            while (numeralsArray.length >= 5) {
-                console.log("Shifted array:", numeralsArray.shift())
-                quotientsArray.push('V');
-                }
-            quotient--;
-        }
-        // const remainder = input % 5;
-        // input = quotient;
-        input--;
-    }
-    console.log('Numerals:', numeralsArray);
-    console.log('Quotients:', quotientsArray);
-    output.innerText = quotientsArray.join('') + numeralsArray.join('');
-    numeralsArray = [];
-    quotientsArray = [];
-
-    // if (numeralsArray.length % 5 === 0) {
-    //     let numOfVs = numeralsArray.length / 5;
-    //     console.log(numOfVs)
-    //     // numeralsArray.forEach((num) => {})
+    ///// SECOND PASS W/O REF OBJ:
+    // while (input > 0) {
+    //     let quotient = Math.floor(input / 5); // number of V's needed
+    //     while (quotient != 0) {
+    //         numeralsArray.push('I');
+    //         while (numeralsArray.length >= 5) {
+    //             console.log("Shifted array:", numeralsArray.shift())
+    //             quotientsArray.push('V');
+    //             }
+    //         quotient--;
+    //     }
+    //     // const remainder = input % 5;
+    //     // input = quotient;
+    //     input--;
     // }
+    // console.log('Numerals:', numeralsArray);
+    // console.log('Quotients:', quotientsArray);
+    // output.innerText = quotientsArray.join('') + numeralsArray.join('');
+    // numeralsArray = [];
+    // quotientsArray = [];
 
-    // numeralsArray.forEach((numeral) => {});
+    ////
 
     // recursive case: n % 5 === 0 i.e. remainder = 0
 
@@ -86,7 +75,7 @@ const arabicToRoman = (input) => {
     //     //
     // }
 
-    // FIRST PASS:
+    ///// FIRST PASS:
     // if (arabicNumber % 10 === 0) {
     //     let quotient = arabicNumber / 10; // number of X's to display
     //     console.log("Quotient: ", quotient)
