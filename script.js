24c1d7336c67d97201be6e3fc4d9a51e415056cc
Elliot <ellthrowaway2@icloud.com>
2024-06-12 13:45:32 -0400

    updated invalid input message logic

--- script.js
+++ script.js
@@ -3,51 +3,72 @@ const input = document.getElementById('number');
 const convertBtn = document.getElementById('convert-btn');
 const output = document.getElementById('output');
 let numeralsArray = [];
-
+let quotientsArray = [];
 
 // FUNCTIONS //
 
 const checkInput = () => {
     const numberInput = input.value;
-    if (!numberInput) {
-        output.innerText = 'Please enter a number';
+    if (!numberInput || numberInput <= 0) {
+        output.innerText = `Please enter ${
+            !numberInput ? 'a number' : 'a number greater than or equal to 1'
+        } `;
     } else {
-        arabicToRoman(numberInput)
+        arabicToRoman(numberInput);
         // output.innerText = numberInput;
-        }
-        output.classList.toggle("hidden");
-        };
-        
-const arabicToRoman = (input) => {
-    let remainder = null;
-    console.log(`Arabic number is: ${input}`);
+    }
+    output.classList.remove('hidden');
+    numeralsArray = [];
+    input.value = '';
+};
 
-    for (let i = input; i > 0; i--){
-        numeralsArray.push("I")
-    };
-    console.log(numeralsArray);
-    output.innerText = numeralsArray.join('')
+const arabicToRoman = (input) => {
+    // let remainder = null;
+
+    // convert input num to all roman I's √√
+    // for (let i = input; i > 0; i--){
+    //     numeralsArray.push("I")
+    //     };
+
+    while (input > 0) {
+        let quotient = Math.floor(input / 5); // number of V's needed
+        while (quotient != 0) {
+            quotientsArray.push('V');
+            quotient--;
+        }
+        // const remainder = input % 5;
+        numeralsArray.push('I');
+        // input = quotient;
+        input--;
+    }
+    console.log('Numerals:', numeralsArray);
+    console.log('Quotients:', quotientsArray);
+    output.innerText = quotientsArray.join('') + numeralsArray.join('');
+
+    // if (numeralsArray.length % 5 === 0) {
+    //     let numOfVs = numeralsArray.length / 5;
+    //     console.log(numOfVs)
+    //     // numeralsArray.forEach((num) => {})
+    // }
 
     // numeralsArray.forEach((numeral) => {});
 
     // recursive case: n % 5 === 0 i.e. remainder = 0
-    
+
     // base case: n % 5 !== 0 i.e. remainder != 0
-    if (input % 5 !== 0) { // input = 17
-        let quotient = Math.floor(input / 5) // num of V's // 3 V's
-        remainder = input % 5 // num of I's // 2 I's
-        // logic to produce n V's ... V * quotient
-        // logic to produce n I's ... I * remainder
+    // if (input % 5 !== 0) { // input = 17
+    //     let quotient = Math.floor(input / 5) // num of V's // 3 V's
+    //     remainder = input % 5 // num of I's // 2 I's
+    //     // logic to produce n V's ... V * quotient
+    //     // logic to produce n I's ... I * remainder
 
-        // is Math.floor(remainder / 5) === 0 ?
-            // Y: return remainder
-    } else { // input % 5 === 0
-        // 
-    }
+    //     // is Math.floor(remainder / 5) === 0 ?
+    //         // Y: return remainder
+    // } else { // input % 5 === 0
+    //     //
+    // }
 
-
-
-// FIRST PASS:
+    // FIRST PASS:
     // if (arabicNumber % 10 === 0) {
     //     let quotient = arabicNumber / 10; // number of X's to display
     //     console.log("Quotient: ", quotient)
@@ -66,17 +87,11 @@ const arabicToRoman = (input) => {
     //     return remainder;
     // };
 
-
-
-
-
     // is n divisible by 5 ? --------- RECURSIVE CASE
-        //// is (n / 5) divisible by 5 ?
-            //// etc.....
+    //// is (n / 5) divisible by 5 ?
+    //// etc.....
     // when n % 5 !== 0, calculate remainder --------- BASE CASE
-
 };
-
 
 // EVENT LISTENERS //
 
@@ -85,4 +100,4 @@ input.addEventListener('keydown', (e) => {
     if (e.key === 'Enter') {
         checkInput();
     }
-})
+});
